f3d018b954a714cd3e93a7294d938360772d92a6
Hancie123 <hanciewanemphago@gmail.com>
2023-09-30 16:42:46 +0545

    user controller added

---
+++ app/Http/Requests/CreateUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+
+      public function rules(): array
+    {
+        return [
+
+            'name'=>['required'],
+            'email'=>['required'],
+            'password'=>['required'],
+            'status'=>['required'],
+        ];
+    }
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
