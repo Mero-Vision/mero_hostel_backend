51ec6842fcdf50d43788f279f3588c55e24e4cfe
Hancie123 <hanciewanemphago@gmail.com>
2023-09-30 21:02:51 +0545

    role added

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
